a576638f5d2e6c2a905c2dc36c64b06433040666
BrysonW <37174771+BrysonW24@users.noreply.github.com>
2023-01-13 14:30:58 +1100

    Update Debug.c

--- Week 1 - C/Debug.c
+++ Week 1 - C/Debug.c
@@ -1,3 +1,15 @@
+Getting Started
+>>Log into code.cs50.io using your GitHub account.
+>>Click inside the terminal window and execute cd.
+>>At the $ prompt, type mkdir debug
+>>Now execute cd debug
+>>Then copy and paste wget https://cdn.cs50.net/2022/fall/labs/1/debug.c into your terminal to download this lab’s distribution code.
+>>Now try compiling this program, by typing make debug and see what happens!
+>>You most likely see an error that says: debug.c:9:5: error: use of undeclared identifier 'name'
+>>This means there is an error that has something to do with an “undeclared identifier”, on line 9. Fix this bug, and then try to compile again. 
+>>Keep in mind that debugging is an iterative process. 
+>>You may need to fix an error, compile, then fix another error, compile again, multiple times!
+
 ORIGINAL CODE:
 // Become familiar wih C syntax
 // Learn to debug buggy code
